b8e27665e70803bebd75bcb0a8e9d069f45d4053
Nikicha <nbozkov871@gmail.com>
2026-05-28 01:30:06 +0300

    Create even or odds.cpp

---
+++ even or odds.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int number ;
+    std::cout << "Enter number :";
+    std::cin >> number;
+    if (number % 2 == 0) {
+        std::cout << " 0 " << std::endl;
+    }
+    else {
+        std::cout << " 1  " << std::endl;
+        
+        
+    }
+    return 0;
+}
